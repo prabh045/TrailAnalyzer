c25c0b0f1e8923c60a2d1d84de8d5971a805899f
PrabhSodhi <prabhdeep045@gmail.com>
2026-08-28 22:20:32 +0530

    added risk card and trail analyser model

--- TrailAnalyser/Models/TrailAnalyzer.swift
+++ TrailAnalyser/Models/TrailAnalyzer.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+import CoreML
+
+struct TrailAnalyzer {
+    func predictRisk() -> Risk {
+        do {
+            let model = try TrailAnalyzerModel(configuration: MLModelConfiguration())
+            let input = TrailAnalyzerModelInput(
+                distance: 1,
+                elevation: 0,
+                terrain: "paved",
+                dangerous: 0
+            )
+            let predictedRisk = try model.prediction(input: input).risk
+            switch predictedRisk {
+            case 0..<20:
+                return .easy
+            case 20..<50:
+                return .moderate
+            case 50..<100:
+                return .difficult
+            default:
+                return .highRisk
+            }
+        }
+        catch {
+            return .highRisk
+        }
+    }
+}
--- TrailAnalyser/Views/RiskCard.swift
+++ TrailAnalyser/Views/RiskCard.swift
@@ -8,11 +8,30 @@
 import SwiftUI
 
 struct RiskCard: View {
+    @State private var risk: Risk
+    
+    init(risk: Risk) {
+        _risk = .init(initialValue: risk)
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading) {
+            HStack {
+                Image(risk.image)
+                    .resizable()
+                    .frame(width: 30, height: 30)
+                Text(risk.rawValue)
+                    .font(.title2)
+            }
+            Text(risk.description)
+                .frame(maxWidth: .infinity, alignment: .leading)
+        }
+        .padding(16)
+        .background(.white, in: RoundedRectangle(cornerRadius: 12))
     }
 }
 
 #Preview {
-    RiskCard()
+    let prediction = TrailAnalyzer().predictRisk()
+    RiskCard(risk: prediction)
 }
