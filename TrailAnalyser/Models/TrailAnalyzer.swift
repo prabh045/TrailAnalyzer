b44ebb8be84d3d17d7a90a4f54e67f4198328f48
PrabhSodhi <prabhdeep045@gmail.com>
2026-08-28 22:20:11 +0530

    added risk card and trail analyser model

---
+++ TrailAnalyser/Models/TrailAnalyzer.swift
@@ -0,0 +1,8 @@
+//
+//  TrailAnalyzer.swift
+//  TrailAnalyser
+//
+//  Created by Prabhdeep Singh on 28/08/26.
+//
+
+import Foundation
---
+++ TrailAnalyser/Views/RiskCard.swift
@@ -0,0 +1,18 @@
+//
+//  RiskCard.swift
+//  TrailAnalyser
+//
+//  Created by Prabhdeep Singh on 28/08/26.
+//
+
+import SwiftUI
+
+struct RiskCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RiskCard()
+}
